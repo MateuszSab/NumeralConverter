7ce76353ab8c3a2aa942872d4a8667bc180d6f77
m.sabatowski <ma.sabatowski@gmail.com>
2022-03-28 11:54:12 +0200

    Automatic recognition

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,15 @@
+import RomanDigit.getAllRomansDigits
+
 import scala.io.StdIn.readLine
 import scala.sys.exit
 
 object Main extends App {
+  val romanDig = List("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
   while (true) {
     val number = readLine("Type your number: ")
-    val question: String = readLine("From what numerical system do you want to convert?(R/ Roman, A/ Arabic): ")
-    if (question == "R") println(NumberConverter.romanToArabic(number))
-    else if (question == "A") println(NumberConverter.arabicToRoman(number.toInt))
-    else println("ERROR! number/system mismatch")
+    if (romanDig.contains(number(0).toString)) println(NumberConverter.romanToArabic(number))
+    else if (number.toIntOption.isDefined) println(NumberConverter.arabicToRoman(number.toInt))
+    else println("ERROR! Invalid input.")
 
     val answer = readLine("Czy kontynuowac? (Y/n) ")
     if (answer == "n") {
@@ -17,6 +19,4 @@ object Main extends App {
     }
   }
 }
-
-
 
