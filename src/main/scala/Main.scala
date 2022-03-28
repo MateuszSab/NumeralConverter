5f1b5ada919de10e4c500c9b64055889b23b2ebc
m.sabatowski <ma.sabatowski@gmail.com>
2022-03-28 11:34:41 +0200

    Main completion

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,22 @@
-object Main extends App{
-  println(NumberConverter.romanToArabic("IIIIIII"))
-  println(NumberConverter.arabicToRoman(26))
+import scala.io.StdIn.readLine
+import scala.sys.exit
 
+object Main extends App {
+  while (true) {
+    val number = readLine("Type your number: ")
+    val question: String = readLine("From what numerical system do you want to convert?(R/ Roman, A/ Arabic): ")
+    if (question == "R") println(NumberConverter.romanToArabic(number))
+    else if (question == "A") println(NumberConverter.arabicToRoman(number.toInt))
+    else println("ERROR! number/system mismatch")
+
+    val answer = readLine("Czy kontynuowac? (Y/n) ")
+    if (answer == "n") {
+      println("Arigato")
+
+      exit()
+    }
+  }
 }
+
+
+
