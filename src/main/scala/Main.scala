b92bf704ba9e891d296d70685e6481adc5fe60c4
m.sabatowski <ma.sabatowski@gmail.com>
2022-03-28 12:03:14 +0200

    comparison fixed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,10 +4,9 @@ import scala.io.StdIn.readLine
 import scala.sys.exit
 
 object Main extends App {
-  val romanDig = List("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
   while (true) {
     val number = readLine("Type your number: ")
-    if (romanDig.contains(number(0).toString)) println(NumberConverter.romanToArabic(number))
+    if (getAllRomansDigits.map(_.toString).contains(number.head.toString)) println(NumberConverter.romanToArabic(number))
     else if (number.toIntOption.isDefined) println(NumberConverter.arabicToRoman(number.toInt))
     else println("ERROR! Invalid input.")
 
